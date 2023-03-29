4018720bb6c30fc4dd55f7a4b4901478475b56da
Kyle Wehrung <kyle.wehrung@gmail.com>
2023-03-28 21:53:43 -0600

    Formik for login

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
-import { Button, Error, Input, FormField} from "../styles";
+import { Button, Error, Input, FormField, Label } from "../styles";
 import styled from "styled-components";
+import { useFormik } from "formik";
+import * as yup from "yup";
 
 const CustomLabel = styled.label`
   color: #4E79D4;
@@ -9,40 +11,53 @@ const CustomLabel = styled.label`
 `;
 
 function LoginForm({ onLogin }) {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-  const [errors, setErrors] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
 
-  function handleSubmit(e) {
-    e.preventDefault();
-    setIsLoading(true);
-    fetch("/login", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ username, password }),
-    }).then((r) => {
-      setIsLoading(false);
-      if (r.ok) {
-        r.json().then((user) => onLogin(user));
-      } else {
-        r.json().then((err) => setErrors(err.errors));
-      }
-    });
-  }
+  const validationSchema = yup.object({
+    username: yup.string().required(),
+    password: yup.string().required(),
+  });
+
+  const formik = useFormik({
+    initialValues: {
+      username: "",
+      password: "",
+    },
+    validationSchema,
+    onSubmit: (values, { setErrors, setSubmitting }) => {
+      setSubmitting(true);
+      fetch("/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(values),
+      })
+      .then((r) => {
+        setSubmitting(false);
+        if (r.ok) {
+          r.json().then((user) => onLogin(user));
+        } else {
+          r.json().then((err) => setErrors(err.errors));
+        }
+      })
+      .catch((error) => {
+        setSubmitting(false);
+        console.log(`Error: ${error}`)
+      });
+    },
+  });
+
 
   return (
-    <form onSubmit={handleSubmit}>
+    <form onSubmit={formik.handleSubmit}>
       <FormField>
         <CustomLabel htmlFor="username">Username</CustomLabel>
         <Input
           type="text"
           id="username"
           autoComplete="off"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
+          value={formik.values.username}
+          onChange={formik.handleChange}
         />
       </FormField>
       <FormField>
@@ -51,17 +66,18 @@ function LoginForm({ onLogin }) {
           type="password"
           id="password"
           autoComplete="current-password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
+          value={formik.values.password}
+          onChange={formik.handleChange}
         />
       </FormField>
       <FormField>
         <Button variant="fill" color="primary" type="submit">
-          {isLoading ? "Loading..." : "Login"}
+          {formik.isSubmitting ? "Loading..." : "Login"}
         </Button>
       </FormField>
       <FormField>
-        {errors.map((err) => (
+        {formik.errors &&
+        Object.values(formik.errors).map((err) => (
           <Error key={err}>{err}</Error>
         ))}
       </FormField>
