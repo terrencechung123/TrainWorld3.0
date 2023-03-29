8eb1766a7a25f149564b041d0859c7d7df5688c2
Kyle Wehrung <kyle.wehrung@gmail.com>
2023-03-28 22:29:40 -0600

    added formik to login and signup form

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Button, Error, Input, FormField, Textarea } from "../styles";
+import { Button, Error, Input, FormField } from "../styles";
 import styled from "styled-components";
 import { useFormik } from "formik";
 import * as yup from "yup";
@@ -10,56 +10,43 @@ const CustomLabel = styled.label`
   font-family: 'Press Start 2P', cursive;
 `;
 
+function LoginForm({ onLogin }) {
 
-function SignUpForm({ onLogin }) {
- 
-  
   const validationSchema = yup.object({
     username: yup.string().required(),
     password: yup.string().required(),
-    passwordConfirmation: yup
-      .string()
-      .oneOf([yup.ref("password"), null], "Passwords must match")
-      .required(),
-    imageUrl: yup.string().required(),
-    bio: yup.string().required(),
   });
-
-
 
   const formik = useFormik({
     initialValues: {
       username: "",
       password: "",
-      passwordConfirmation: "",
-      imageUrl: "",
-      bio: "",
     },
     validationSchema,
     onSubmit: (values, { setErrors, setSubmitting }) => {
       setSubmitting(true);
-      fetch("/signup", {
+      fetch("/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
         },
         body: JSON.stringify(values),
       })
-        .then((r) => {
-          setSubmitting(false);
-          if (r.ok) {
-            r.json().then((user) => onLogin(user));
-          } else {
-            r.json().then((err) => setErrors(err.errors));
-
-          }
-        })
-        .catch((error) => {
-          setSubmitting(false);
-          console.error(error);
-        });
+      .then((r) => {
+        setSubmitting(false);
+        if (r.ok) {
+          r.json().then((user) => onLogin(user));
+        } else {
+          r.json().then((err) => setErrors(err.errors));
+        }
+      })
+      .catch((error) => {
+        setSubmitting(false);
+        console.log(`Error: ${error}`)
+      });
     },
   });
+
 
   return (
     <form onSubmit={formik.handleSubmit}>
@@ -78,53 +65,24 @@ function SignUpForm({ onLogin }) {
         <Input
           type="password"
           id="password"
+          autoComplete="current-password"
           value={formik.values.password}
           onChange={formik.handleChange}
-          autoComplete="current-password"
         />
       </FormField>
       <FormField>
-        <CustomLabel htmlFor="passwordcConfirmation">Password Confirmation</CustomLabel>
-        <Input
-          type="password"
-          id="passwordConfirmation"
-          value={formik.values.passwordConfirmation}
-          onChange={formik.handleChange}
-          autoComplete="current-password"
-        />
-      </FormField>
-      <FormField>
-        <CustomLabel htmlFor="imageUrl">Profile Image</CustomLabel>
-        <Input
-          type="text"
-          id="imageUrl"
-          value={formik.values.imageUrl}
-          onChange={formik.handleChange}
-        />
-      </FormField>
-      <FormField>
-        <CustomLabel htmlFor="bio">Bio</CustomLabel>
-        <Textarea
-          rows="3"
-          id="bio"
-          value={formik.values.bio}
-          onChange={formik.handleChange}
-        />
-      </FormField>
-      <FormField>
-      <Button type="submit" disabled={formik.isSubmitting}>
-          {formik.isSubmitting ? "Loading..." : "Sign Up"}
+        <Button variant="fill" color="primary" type="submit">
+          {formik.isSubmitting ? "Loading..." : "Login"}
         </Button>
       </FormField>
       <FormField>
         {formik.errors &&
-          Object.values(formik.errors).map((err) => (
-            <Error key={err}>{err}</Error>
-          ))
-        }
-        </FormField>
+        Object.values(formik.errors).map((err) => (
+          <Error key={err}>{err}</Error>
+        ))}
+      </FormField>
     </form>
   );
 }
 
-export default SignUpForm;
+export default LoginForm;
